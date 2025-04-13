7cf855dd8132d30fec1a822bdbd72eba9cebe3dd
CercleTour32576 <cercletour@gmail.com>
2025-04-13 10:42:36 +0200

    Exo 2 fonctionnel

--- exercise_2.c
+++ exercise_2.c
@@ -22,28 +22,147 @@
 /****************************************************/
 #include "src/lbm_struct.h"
 #include "src/exercises.h"
+#include "mpi.h"
+#include <stdbool.h>
 
 /****************************************************/
 void lbm_comm_init_ex2(lbm_comm_t * comm, int total_width, int total_height)
 {
-	//we use the same implementation then ex1
-	lbm_comm_init_ex1(comm, total_width, total_height);
+	//
+	// TODO: calculate the splitting parameters for the current task.
+	//
+	// HINT: You can look in exercise_0.c to get an example for the sequential case.
+	//
+	int rank;
+	int comm_size;
+	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	MPI_Comm_size( MPI_COMM_WORLD, &comm_size );
+	
+	if (total_width % comm_size != 0) {
+		char error_message[256];
+		sprintf(error_message, "Invalid communicator size ! (%d / %d is not an integer)", comm_size, total_width);
+		fatal(error_message);
+	}
+	comm->nb_x = comm_size;
+	comm->nb_y = 1;
+	
+	comm->rank_x = rank;
+	comm->rank_y = 0;
+	
+	// TODO : calculate the local sub-domain size (do not forget the 
+	//        ghost cells). Use total_width & total_height as starting 
+	//        point.
+	comm->width = total_width/comm_size + 2;
+	comm->height = total_height + 2;
+	// + 2 for ghost cells
+	
+	// TODO : calculate the absolute position in the global mesh.
+	//        without accounting the ghost cells
+	//        (used to setup the obstable & initial conditions).
+	comm->x = (comm->width-2) * rank;
+	comm->y = 0;
+	
+	//if debug print comm
+	lbm_comm_print(comm);
 }
 
 /****************************************************/
 void lbm_comm_ghost_exchange_ex2(lbm_comm_t * comm, lbm_mesh_t * mesh)
 {
-	//
-	// TODO: Implement the 1D communication with blocking MPI functions using
-	//       odd/even communications.
-	//
-	// To be used:
-	//    - DIRECTIONS: the number of doubles composing a cell
-	//    - double[9] lbm_mesh_get_cell(mesh, x, y): function to get the address of a particular cell.
-	//    - comm->width : The with of the local sub-domain (containing the ghost cells)
-	//    - comm->height : The height of the local sub-domain (containing the ghost cells)
+	bool first = comm->rank_x == 0;
+	bool last = comm->rank_x == comm->nb_x-1;
+
+	int height = comm->height;
+	int width = comm->width;
+
+	int rank = comm->rank_x;
+
+	if (rank % 2 != 0) { // ODE = Send befor
+		// Send to right
+		if (!last) {
+			for (int i = 0; i < height; i++) {
+				MPI_Send(
+					lbm_mesh_get_cell(mesh, width - 2, i),
+					DIRECTIONS,
+					MPI_DOUBLE,
+					rank + 1,
+					321,
+					MPI_COMM_WORLD
+				);
+			}
+		}
+		
+		// Send to left
+		if (!first) {
+			for (int i = 0; i < height; i++) {
+				MPI_Send(
+					lbm_mesh_get_cell(mesh, 1, i),
+					DIRECTIONS,
+					MPI_DOUBLE,
+					rank - 1,
+					123,
+					MPI_COMM_WORLD
+				);
+			}
+		}
+	}
 	
-	//example to access cell
-	//double * cell = lbm_mesh_get_cell(mesh, local_x, local_y);
-	//double * cell = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+	// Receive from left
+	if (!first) {
+		for (int i = 0; i < height; i++) {
+			MPI_Recv(
+				lbm_mesh_get_cell(mesh, 0, i),
+				DIRECTIONS,
+				MPI_DOUBLE,
+				rank - 1,
+				321,
+				MPI_COMM_WORLD,
+				MPI_STATUS_IGNORE
+			);
+		}
+	}
+	
+	// Receive from right
+	if (!last) {
+		for (int i = 0; i < height; i++) {
+			MPI_Recv(
+				lbm_mesh_get_cell(mesh, width - 1, i),
+				DIRECTIONS,
+				MPI_DOUBLE,
+				rank + 1,
+				123,
+				MPI_COMM_WORLD,
+				MPI_STATUS_IGNORE
+			);
+		}
+	}
+	if (rank % 2 == 0) { // EVEN : send after
+		// Send to right
+		if (!last) {
+			for (int i = 0; i < height; i++) {
+				MPI_Send(
+					lbm_mesh_get_cell(mesh, width - 2, i),
+					DIRECTIONS,
+					MPI_DOUBLE,
+					rank + 1,
+					321,
+					MPI_COMM_WORLD
+				);
+			}
+		}
+		
+		// Send to left
+		if (!first) {
+			for (int i = 0; i < height; i++) {
+				MPI_Send(
+					lbm_mesh_get_cell(mesh, 1, i),
+					DIRECTIONS,
+					MPI_DOUBLE,
+					rank - 1,
+					123,
+					MPI_COMM_WORLD
+				);
+			}
+		}
+	}
 }
